c56aced935c3d8a0d246335686174ebc1dcefb6f
yonash24 <yonashay87@gmail.com>
2025-07-13 21:03:59 +0300

    add filter and aggregation and summary functions

--- DataHandler.py
+++ DataHandler.py
@@ -29,13 +29,13 @@ class Data:
         
         if data_file:
             try:
-                os.rename(data_file,"videoGame2024_dataset.csv")
+                os.rename(data_file,"videoGame_dataset.csv")
             except FileNotFoundError:
                 print(f"the dataset file not found")
             except Exception as e:
                 print(f"an error occured {e}")
                 
-            data_set_frame = pd.read_csv("videoGame2024_dataset.csv")
+            data_set_frame = pd.read_csv("videoGame_dataset.csv")
             return data_set_frame
         
         
@@ -51,34 +51,37 @@ class Data:
     @staticmethod
     def data_to_csv(dataset:pd.DataFrame):
         try:
-            dataset.to_csv("videoGame2024_dataset", index= False)
+            dataset.to_csv("videoGame_dataset.csv", index= False)
         except Exception as e:
             print(f"exception {e} has occured")
         
     #sort the dataset by paarameter
     @staticmethod
-    def dort_data(dataset:pd.DataFrame):
-        param = input("choos a parameter to sort by him the data: console\n publisher\n developer\n critic_score\n total_sales\n na_sales\n  jp_sales\n pal_sales\n release_date\n last_update")
+    def sort_data(param,dataset:pd.DataFrame):
         if param == "console":
-            dataset.sort_values(by="console", ascending=False, inplace=True)
+            dataset = dataset.sort_values(by="console", ascending=False)
+        elif param == "title":
+            dataset = dataset.sort_values(by="title", ascending=False)
+        elif param == "genre":
+            dataset = dataset.sort_values(by="genre", ascending=False)
         elif param == "publisher":
-            dataset.sort_values(by="publisher", ascending=False, inplace=True)    
+            dataset = dataset.sort_values(by="publisher", ascending=False)    
         elif param == "developer":
-            dataset.sort_values(by="developer", ascending=False, inplace=True)
+            dataset = dataset.sort_values(by="developer", ascending=False)
         elif param == "critic_score":
-            dataset.sort_values(by="critic_score", ascending=False, inplace=True)    
+            dataset = dataset.sort_values(by="critic_score", ascending=False)    
         elif param == "total_sales":
-            dataset.sort_values(by="total_sales", ascending=False, inplace=True)
+            dataset = dataset.sort_values(by="total_sales", ascending=False)
         elif param == "na_sales":
-            dataset.sort_values(by="na_sales", ascending=False, inplace=True) 
+            dataset = dataset.sort_values(by="na_sales", ascending=False) 
         elif param == "jp_sales":
-            dataset.sort_values(by="jp_sales", ascending=False, inplace=True) 
+            dataset = dataset.sort_values(by="jp_sales", ascending=False) 
         elif param == "pal_sales":
-            dataset.sort_values(by="pal_sales", ascending=False, inplace=True) 
+            dataset = dataset.sort_values(by="pal_sales", ascending=False) 
         elif param == "release_date":
-            dataset.sort_values(by="release_date", ascending=False, inplace=True)    
+            dataset = dataset.sort_values(by="release_date", ascending=False)    
         elif param == "last_update":
-            dataset.sort_values(by="last_update", ascending=False, inplace=True)
+            dataset = dataset.sort_values(by="last_update", ascending=False)
         else:
             print("invalid input please enter one of the following parameters from above")
         return dataset    
@@ -86,6 +89,73 @@ class Data:
     #get the amout of elemant you want the type of info and a dataset
     #retrun a dataframe at the same size by the required data from the dataset
     @staticmethod
-    def get_data(self,size,info,dataset:pd.DataFrame):
-        pass
+    def get_data(size,info,dataset:pd.DataFrame):
+        sorted_data = Data.sort_data(info,dataset)
+        new_data = sorted_data[:size]
+        return new_data
+    
+    
+    #return the number of sales of a company
+    @staticmethod
+    def get_sum_of_sales(publisher, dataset:pd.DataFrame):
+        company_data = dataset[dataset['publisher'] == publisher]
+        sum = company_data['total_sales'].sum()
+        return sum
+    
+    
+    #how many a games certain company published on a certain year
+    @staticmethod
+    def sold_in_year(year,company,dataset:pd.DataFrame):
+        company_data = dataset[dataset['publisher'] == company]
+        year_data = pd.to_datetime(company_data['release_date'], errors='coerce').dt.year
+        new_dataset = company_data[year_data == year]
+        num_of_games = len(new_dataset)
+        return num_of_games
+    
+    
+    #get the top n rows by total_sales
+    @staticmethod
+    def get_top_n_sales(n, dataset:pd.DataFrame):
+        sorted_data = Data.sort_data("total_sales",dataset)
+        new_dataset = sorted_data[:n]
+        return new_dataset
+    
+    #Calculates the average critic score for games by a specific publisher
+    @staticmethod
+    def avg_critic_score(publisher, dataset:pd.DataFrame):
+        publisher_data = dataset[dataset['publisher'] == publisher]
+        avg = publisher_data['critic_score'].fillna(0).mean()
+        return avg
+    
+    #Counts how many games belong to each genre
+    @staticmethod
+    def count_by_genre(dataset:pd.DataFrame):
+        count = dataset['genre'].value_counts()        
+        return count
+    
+    
+    #convert sales amout to millions
+    @staticmethod
+    def to_millions(column, dataset:pd.DataFrame):
+        if column not in ("critic_score", "total_sales", "na_sales", "jp_sales", "pal_sales","other_sales"):
+            print("invalid column to convert to millions")
+            return
         
+        dataset[column] = dataset[column].mul(1000000)
+        return dataset
+    
+    
+    #how many games belong to each company
+    @staticmethod
+    def company_games(dataset:pd.DataFrame):
+        count = dataset['publisher'].value_counts()
+        return count
+    
+    #Returns all games released on a specific platform.
+    @staticmethod
+    def games_on_platforms(platform, dataset:pd.DataFrame):
+        console = dataset[dataset['console'] == platform]
+        count = len(console)
+        return count
+    
+    
---
+++ VideoGameVisualizer.py
@@ -0,0 +1,7 @@
+import DataHandler
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#class to visualize the data
+class visualize:
+    pass
